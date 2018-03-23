3843c48dc32a010f00cfc84fbc862354e740c2ce
Daniel Schmidt <dschmidt@mesosphere.com>
2018-03-23 16:49:00 +0100

    test: setup jest
    We can't use story shots because of other plugins we use. Maybe we
    should take the time to decide if
    a) this is important to us and
    b) we can find a way around this
    
    Closes DCOS-21785

---
+++ components/badge/badge.test.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Badge from '../badge';
+import renderer from 'react-test-renderer';
+
+describe("Badge", () => {
+  it("default", () => {
+    expect(renderer
+    .create(<Badge>default</Badge>)
+    .toJSON()).toMatchSnapshot()
+  });
+
+  it("success", () => {
+    expect(renderer
+    .create(<Badge appearance="success">success</Badge>)
+    .toJSON()).toMatchSnapshot()
+  });
+
+  it("information", () => {
+    expect(renderer
+    .create(<Badge appearance="information">information</Badge>)
+    .toJSON()).toMatchSnapshot()
+  });
+
+
+  it("warning", () => {
+    expect(renderer
+    .create(<Badge appearance="warning">warning</Badge>)
+    .toJSON()).toMatchSnapshot()
+  });
+
+  it("danger", () => {
+    expect(renderer
+    .create(<Badge appearance="danger">danger</Badge>)
+    .toJSON()).toMatchSnapshot()
+  });
+});
