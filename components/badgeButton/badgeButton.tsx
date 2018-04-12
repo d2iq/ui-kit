9acdadc985283cb458b73399c08e524600bd1ea7
Poltergeist <philipph@mesosphere.io>
2018-04-12 11:21:02 +0200

    feat(badgebutton): add on click property to badgebutton
    This adds the on click property to the badgebutton and changes the element from a span to a button.
    This also changes the style so that the badge button looks similar to the badge.

--- components/badgeButton/badgeButton.tsx
+++ components/badgeButton/badgeButton.tsx
@@ -2,19 +2,34 @@ import * as React from "react";
 import { badgeButton } from "./style";
 
 export interface IBadgeButtonProps {
-  appearance?: "default" | "success" | "primary" | "warning" | "danger" | "outline";
+  appearance?:
+    | "default"
+    | "success"
+    | "primary"
+    | "warning"
+    | "danger"
+    | "outline";
+  onClick?: (...args: any[]) => void;
+  /**
+   * Tab index indicates if an element can be focuesed for more information see
+   * https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/tabindex
+   * browser default value for this is -1
+   */
+  tabIndex?: number;
   children: JSX.Element | string;
 }
 
 export class BadgeButton extends React.PureComponent<IBadgeButtonProps, {}> {
   public static defaultProps: Partial<IBadgeButtonProps> = {
-    appearance: "default"
+    appearance: "default",
+    tabIndex: -1
   };
 
   public render() {
-    const { appearance, children } = this.props;
+    const { appearance, children, onClick, tabIndex } = this.props;
+    const props = { onClick, tabIndex, className: badgeButton(appearance) };
 
-    return <span className={badgeButton(appearance)}>{children}</span>;
+    return <button {...props}>{children}</button>;
   }
 }
 
--- components/badgeButton/stories/badgeButton.stories.tsx
+++ components/badgeButton/stories/badgeButton.stories.tsx
@@ -2,37 +2,53 @@ import * as React from "react";
 import { storiesOf } from "@storybook/react";
 import { withReadme } from "storybook-readme";
 import { BadgeButton } from "../../index";
+import { action } from "@storybook/addon-actions";
 
 const BadgeButtonReadme = require("../README.md");
 
 storiesOf("BadgeButton", module)
   .addDecorator(withReadme([BadgeButtonReadme]))
-  .addWithInfo(
-    "Default",
-    () => <BadgeButton>Default</BadgeButton>
-  )
-  .addWithInfo(
-    "Success",
-    "Represents something positive.",
-    () => <BadgeButton appearance="success">Success</BadgeButton>
-  )
+  .addWithInfo("Default", () => (
+    <BadgeButton onClick={action("BadgeButton pressed")}>Default</BadgeButton>
+  ))
+  .addWithInfo("Success", "Represents something positive.", () => (
+    <BadgeButton onClick={action("BadgeButton Pressed")} appearance="success">
+      Success
+    </BadgeButton>
+  ))
   .addWithInfo(
     "Primary",
     "Represents something more significant than a default.",
-    () => <BadgeButton appearance="primary">Primary</BadgeButton>
+    () => (
+      <BadgeButton onClick={action("BadgeButton Pressed")} appearance="primary">
+        Primary
+      </BadgeButton>
+    )
   )
   .addWithInfo(
     "Danger",
     "Represents something in a danger or error state.",
-    () => <BadgeButton appearance="danger">Danger</BadgeButton>
+    () => (
+      <BadgeButton onClick={action("BadgeButton Pressed")} appearance="danger">
+        Danger
+      </BadgeButton>
+    )
   )
   .addWithInfo(
     "Warning",
     "Represents something that we want to warn the user about but not quite extreme as a danger state.",
-    () => <BadgeButton appearance="warning">Warning</BadgeButton>
+    () => (
+      <BadgeButton onClick={action("BadgeButton Pressed")} appearance="warning">
+        Warning
+      </BadgeButton>
+    )
   )
   .addWithInfo(
     "Outline",
     "Outline badgeButtons for when we want the density of the badgeButton to be lighter e.g. when next to data in a table cell",
-    () => <BadgeButton appearance="outline">Outline</BadgeButton>
-  )
+    () => (
+      <BadgeButton onClick={action("BadgeButton Pressed")} appearance="outline">
+        Outline
+      </BadgeButton>
+    )
+  );
