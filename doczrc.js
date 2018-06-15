7bda6f6e8fcd433814824b30161e73bcceefa40b
Poltergeist <philipph@mesosphere.io>
2018-06-15 12:19:55 +0200

    chore(docz): add docz package and remove storybook

---
+++ doczrc.js
@@ -0,0 +1,3 @@
+export default {
+  menu: ["Readme", "Contributing"]
+};
--- packages/shared/icons.tsx
+++ packages/shared/icons.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import * as React from 'react';
 
 // TODO: once we have an icon library, use that instead
 export const DownTriangle = () => (
