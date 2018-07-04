655e6b8b21397282a263853f74d7e3039822aaff
Poltergeist <philipph@mesosphere.io>
2018-07-04 17:20:49 +0200

    feat(table): add header cell component
    This adds a HeaderCell Component which provides basic styling for table headers.

--- packages/index.ts
+++ packages/index.ts
@@ -1,3 +1,3 @@
 export { Badge } from "./badge";
 export { BadgeButton } from "./badge";
-export { Column, Table, Cell } from "./table";
+export { Column, Table, Cell, HeaderCell } from "./table";
--- packages/table/index.ts
+++ packages/table/index.ts
@@ -1,3 +1,4 @@
 export { default as Table } from "./components/Table";
 export { default as Column } from "./components/Column";
 export { default as Cell } from "./components/Cell";
+export { default as HeaderCell } from "./components/HeaderCell";
