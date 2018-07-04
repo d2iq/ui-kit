bb75c363b0873db8b487fb34e971a82f5c1127ea
Poltergeist <philipph@mesosphere.io>
2018-07-04 17:22:08 +0200

    feat(table): add text cell component
    This adds a TextCell component which provides basic styling for text cells. Specifically
    `text-transform: ellipsis;`

--- packages/index.ts
+++ packages/index.ts
@@ -1,3 +1,3 @@
 export { Badge } from "./badge";
 export { BadgeButton } from "./badge";
-export { Column, Table, Cell, HeaderCell } from "./table";
+export { Column, Table, Cell, TextCell, HeaderCell } from "./table";
