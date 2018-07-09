6c9b2c6da872a30c15b468c5d98bbf1154b8ee46
Daniel Schmidt <dschmidt@mesosphere.com>
2018-07-09 15:35:20 +0200

    chore(table): add story for dynamic data

--- packages/table/stories/Table.stories.tsx
+++ packages/table/stories/Table.stories.tsx
@@ -63,9 +63,45 @@ const veryLongRenderer = () => (
     </span>
   </TextCell>
 );
+
 const empty = () => <Cell>empty</Cell>;
 const width = ({ width: totalWidth }: { width?: number }) =>
   totalWidth ? totalWidth * 0.3 : 100;
+
+const itemCellRenderer = item => (
+  <Cell>
+    <span>{item}</span>
+  </Cell>
+);
+
+const randomNumbers = n => new Array(n).fill(0).map(() => Math.random() * 100);
+
+class ChangingTable extends React.Component<{}, { items: number[] }> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: randomNumbers(20)
+    };
+
+    setInterval(() => {
+      this.setState({
+        items: randomNumbers(20)
+      });
+    }, 100);
+  }
+
+  render() {
+    return (
+      <Table data={this.state.items}>
+        <Column
+          header={<HeaderCell>name</HeaderCell>}
+          cellRenderer={itemCellRenderer}
+          width={width}
+        />
+      </Table>
+    );
+  }
+}
 
 storiesOf("Table", module)
   .addDecorator(withReadme([readme]))
@@ -110,5 +146,16 @@ storiesOf("Table", module)
           width={width}
         />
       </Table>
+    </div>
+  ))
+  .addWithInfo("changing values", () => (
+    <div
+      style={{
+        height: "175px",
+        width: "100%",
+        fontSize: "14px"
+      }}
+    >
+      <ChangingTable />
     </div>
   ));
