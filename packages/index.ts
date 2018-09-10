c946045c504dfc27df53698f4865651eb9fae94a
Mike Perrotti <mperrotti573@gmail.com>
2018-09-10 11:27:15 -0400

    fix: forgot to export sortableheadercell in initial PR

--- packages/index.ts
+++ packages/index.ts
@@ -1,6 +1,14 @@
 import { injectGlobalCss } from "./shared/styles/global";
 export { Badge, BadgeButton } from "./badge";
-export { Column, Table, Cell, TextCell, HeaderCell, NumberCell } from "./table";
+export {
+  Column,
+  Table,
+  Cell,
+  TextCell,
+  HeaderCell,
+  SortableHeaderCell,
+  NumberCell
+} from "./table";
 export { ToggleContent } from "./toggleContent";
 
 injectGlobalCss();
