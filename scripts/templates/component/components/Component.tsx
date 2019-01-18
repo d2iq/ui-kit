a6245c079c9ccd3947a4980c18042722248f2b0e
Mike Perrotti <mperrotti573@gmail.com>
2019-01-17 20:43:19 -0500

    chore: updated component generator

--- scripts/templates/component/components/Component.tsx
+++ scripts/templates/component/components/Component.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
 import { style } from "../style";
 
-export interface I${Component}Props {
+export interface ${Component}Props {
   children?: React.ReactNode | string;
 }
 
-class ${Component} extends React.PureComponent<I${Component}Props, {}> {
+class ${Component} extends React.PureComponent<${Component}Props, {}> {
   public render() {
     const { children } = this.props;
 
