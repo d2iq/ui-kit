43879c963e9f3be09bbc076a791afa9b77281219
Poltergeist <philipph@mesosphere.io>
2019-05-21 13:52:04 +0200

    chore(scripts): fix create component script to support new storybook version

--- scripts/templates/component/stories/Component.stories.tsx
+++ scripts/templates/component/stories/Component.stories.tsx
@@ -1,10 +1,10 @@
 import * as React from "react";
 import { storiesOf } from "@storybook/react";
 import { withReadme } from "storybook-readme";
-import { ${Component} } from "../../index";
+import { ${Component} } from "../index";
 
 const readme = require("../README.md");
 
 storiesOf("${Component}", module)
   .addDecorator(withReadme([readme]))
-  .addWithInfo("default", () => <${Component}>default</${Component}>)
+  .add("default", () => <${Component}>default</${Component}>)
