ba73849e2d478800a0c8d8252b1cb9d4c96f1860
Mike Perrotti <mperrotti573@gmail.com>
2019-06-27 13:28:15 -0400

    feat(avatar): adds an avatar component
    Closes DCOS-55837

---
+++ packages/avatar/style.ts
@@ -0,0 +1,22 @@
+import { css } from "emotion";
+import { AvatarSizes } from "./components/Avatar";
+import { themeBgSecondary } from "../design-tokens/build/js/designTokens";
+
+export const avatarContainer = css`
+  background-color: ${themeBgSecondary};
+  border-radius: 25%;
+  display: inline-block;
+  overflow: hidden;
+`;
+
+export const avatarSize = (size: AvatarSizes) => css`
+  height: ${size};
+  width: ${size};
+`;
+
+export const avatarImg = css`
+  border: 0;
+  height: 100%;
+  object-fit: cover;
+  width: 100%;
+`;
