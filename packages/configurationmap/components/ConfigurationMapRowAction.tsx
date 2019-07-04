2807665fc67a49389cd87b4b1bd315e8e729c3ac
Mike Perrotti <mperrotti573@gmail.com>
2019-07-04 01:18:05 -0400

    feat(styleutilities): adds layout components
    These are the style utility components that help us with acheiving common layout patterns

---
+++ packages/configurationmap/components/ConfigurationMapRowAction.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+import SecondaryButton from "../../button/components/SecondaryButton";
+import { cx } from "react-emotion";
+import { flexItem } from "../../shared/styles/styleUtils";
+
+interface ConfigurationMapRowActionProps {
+  /**
+   * What to render as the action content
+   */
+  children: React.ReactNode;
+  /**
+   * Click handler for the action
+   */
+  onClick: (event?: React.SyntheticEvent<HTMLElement>) => void;
+}
+
+export const rowActionStaticClassname = "static_configurationMapRowAction";
+
+const ConfigurationMapRowAction: React.StatelessComponent<
+  ConfigurationMapRowActionProps
+> = props => {
+  const { onClick, children } = props;
+
+  return (
+    <span className={cx(rowActionStaticClassname, flexItem("shrink"))}>
+      <SecondaryButton onClick={onClick}>{children}</SecondaryButton>
+    </span>
+  );
+};
+
+export default ConfigurationMapRowAction;
---
+++ packages/shared/styles/styleUtils/resets/definitionListReset.ts
@@ -0,0 +1,10 @@
+import { css } from "emotion";
+
+export const dlReset = css`
+  margin-top: 0;
+  margin-bottom: 0;
+`;
+
+export const ddReset = css`
+  margin-left: 0;
+`;
