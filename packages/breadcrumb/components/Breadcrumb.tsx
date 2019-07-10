bca2e57905601bc93b6fa862b7677ec4035c3d23
Pierre Beitz <pierrebeitz@gmail.com>
2019-07-10 10:18:21 +0200

    refactor(breadcrumb): for readability
    we now have an intersperse-function that communicates that we want to put
    separators inbetween the elements. also we changed to using padding-left instead
    of padding right to make the special case of NOT needing padding for the 1st
    instead of the last element, which reduces mental overhead (at least for me).
    compare:
    
    - [padding("right", "xs")]: index !== crumbsArr.length - 1
    + [padding("left", "xs")]: i !== 0

--- packages/breadcrumb/components/Breadcrumb.tsx
+++ packages/breadcrumb/components/Breadcrumb.tsx
@@ -15,39 +15,32 @@ export interface BreadcrumbProps {
   children?: React.ReactNode | string;
 }
 
+function intersperse<A>(list: A[], sep: JSX.Element) {
+  return Array.prototype.concat(...list.map(e => [sep, e])).slice(1);
+}
+
 class Breadcrumb extends React.PureComponent<BreadcrumbProps, {}> {
   public render() {
-    const crumbsArr = React.Children.toArray(this.props.children);
     const breadcrumbSeparator = (
       <Icon shape={SystemIcons.CaretRight} size={iconSizeXs} />
+    );
+    const crumbsArr = intersperse(
+      React.Children.toArray(this.props.children),
+      breadcrumbSeparator
     );
 
     return (
       <nav className={cx(flex({ align: "center", wrap: "wrap" }))}>
-        {crumbsArr.reduce(
-          (acc, crumb, index) =>
-            acc.concat([
-              <div
-                className={cx(textWeight("medium"), textSize("l"), {
-                  [padding("right", "xs")]: index !== crumbsArr.length - 1
-                })}
-                key={`breadcrumb-wrapper-${index}`}
-              >
-                {crumb}
-              </div>,
-              index === crumbsArr.length - 1 ? null : (
-                <div
-                  className={cx(textWeight("medium"), textSize("l"), {
-                    [padding("right", "xs")]: index !== crumbsArr.length - 1
-                  })}
-                  key={`breadcrumb-seperator-${index}`}
-                >
-                  {breadcrumbSeparator}
-                </div>
-              )
-            ]),
-          Array()
-        )}
+        {crumbsArr.map((crumb, i) => (
+          <div
+            key={`breadcrumb-wrapper-${i}`}
+            className={cx(textWeight("medium"), textSize("l"), {
+              [padding("left", "xs")]: i !== 0
+            })}
+          >
+            {crumb}
+          </div>
+        ))}
       </nav>
     );
   }
