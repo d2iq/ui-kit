e6a945b00e098c305c540768fdd74debabf3212a
Mike Perrotti <mperrotti573@gmail.com>
2019-07-11 19:48:02 -0400

    feat(card): adds aspectRatio prop

--- packages/card/stories/Card.stories.tsx
+++ packages/card/stories/Card.stories.tsx
@@ -7,4 +7,11 @@ const readme = require("../README.md");
 
 storiesOf("Card", module)
   .addDecorator(withReadme([readme]))
-  .add("default", () => <Card>default</Card>);
+  .add("default", () => <Card>default</Card>)
+  .add("2:1 aspect ratio", () => {
+    return (
+      <div style={{ maxWidth: "400px" }}>
+        <Card aspectRatio={[2, 1]}>I stay at a 2:1 aspect ratio</Card>
+      </div>
+    );
+  });
--- packages/card/style.ts
+++ packages/card/style.ts
@@ -1,14 +1,9 @@
 import { css } from "emotion";
-import {
-  border,
-  borderRadius,
-  padding
-} from "../shared/styles/styleUtils/index";
+import { border, borderRadius } from "../shared/styles/styleUtils/index";
 import { themeBgPrimary } from "../design-tokens/build/js/designTokens";
 
 export const style = css`
   background-color: ${themeBgPrimary};
   ${border("all")};
   ${borderRadius("default")};
-  ${padding("all")};
 `;
