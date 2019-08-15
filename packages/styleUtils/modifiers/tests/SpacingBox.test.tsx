2ee605314d41ca5c81aa933c33af55c1dd359a01
Mike Perrotti <mperrotti573@gmail.com>
2019-08-15 19:46:37 -0400

    feat(spacingbox): support different spacing sizes per side

--- packages/styleUtils/modifiers/tests/SpacingBox.test.tsx
+++ packages/styleUtils/modifiers/tests/SpacingBox.test.tsx
@@ -36,4 +36,52 @@ describe("SpacingBox", () => {
 
     expect(toJson(component)).toMatchSnapshot();
   });
+
+  it("renders with different spacing on each side", () => {
+    const component = shallow(
+      <SpacingBox
+        tag="div"
+        spacingSizePerSide={{
+          top: "none",
+          right: "xs",
+          bottom: "s",
+          left: "m"
+        }}
+      >
+        content
+      </SpacingBox>
+    );
+
+    expect(toJson(component)).toMatchSnapshot();
+  });
+
+  it("renders with responsive spacing on each side", () => {
+    const component = shallow(
+      <SpacingBox
+        tag="div"
+        spacingSizePerSide={{
+          top: {
+            default: "none",
+            medium: "xs"
+          },
+          right: {
+            default: "none",
+            medium: "s"
+          },
+          bottom: {
+            default: "none",
+            medium: "m"
+          },
+          left: {
+            default: "none",
+            medium: "l"
+          }
+        }}
+      >
+        content
+      </SpacingBox>
+    );
+
+    expect(toJson(component)).toMatchSnapshot();
+  });
 });
