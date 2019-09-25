6b3eb96db10fec9d6d7f4ca543efbc0cad8c4c94
Mike Perrotti <mperrotti573@gmail.com>
2019-09-25 17:11:16 -0400

    feat: adds subtle border around avatar

--- packages/avatar/style.ts
+++ packages/avatar/style.ts
@@ -1,9 +1,13 @@
 import { css } from "emotion";
 import { AvatarSizes } from "./components/Avatar";
-import { themeBgSecondary } from "../design-tokens/build/js/designTokens";
+import {
+  themeBgSecondary,
+  greyLightLighten5
+} from "../design-tokens/build/js/designTokens";
 
 export const avatarContainer = css`
   background-color: ${themeBgSecondary};
+  border: 1px solid ${greyLightLighten5};
   border-radius: 25%;
   overflow: hidden;
   vertical-align: middle;
