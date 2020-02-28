f7964ef5885cb3a3bb52f0ac6440b93bb8bac0d0
Mike Perrotti <mperrotti573@gmail.com>
2020-02-28 18:20:13 -0500

    fix: uses typography components in ConfigMap
    These changes also include some spacing adjustments.
    
    Closes D2IQ-64547

--- packages/configurationmap/components/ConfigurationMap.tsx
+++ packages/configurationmap/components/ConfigurationMap.tsx
@@ -1,11 +1,5 @@
 import * as React from "react";
-import { padding } from "../../shared/styles/styleUtils";
-import styled from "react-emotion";
-
-const ConfigurationMap = styled("div")`
-  ${padding("bottom", "l")};
-`;
 
 export default ({ children }) => (
-  <ConfigurationMap data-cy="configurationMap">{children}</ConfigurationMap>
+  <div data-cy="configurationMap">{children}</div>
 );
