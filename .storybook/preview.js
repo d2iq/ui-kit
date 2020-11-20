96eaa6a2c97959e26a207546bf667249068f9416
Rodney Norris <rnorris@d2iq.com>
2020-11-20 13:11:56 -0600

    feat: sb v5.3 update
    - Closes D2IQ-66047

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,7 @@
-const req = require.context("./../packages", true, /.stories.tsx$/);
-
-import { configure, addDecorator, addParameters } from "@storybook/react";
+import { addDecorator } from "@storybook/react";
 import { withKnobs } from "@storybook/addon-knobs/react";
 import { withA11y } from "@storybook/addon-a11y";
 import { withInfo } from "@storybook/addon-info";
-import { create } from '@storybook/theming/create';
 import { addReadme } from 'storybook-readme';
 import { iconDecorator } from "../decorators/iconDecorator";
 require("../packages/shared/styles/global").injectStorybookResetCss();
@@ -19,20 +16,3 @@ addDecorator(
     source: false
   })
 );
-addParameters({
-  options: {
-    isFullscreen: false,
-    showPanel: true,
-    panelPosition: "bottom",
-    theme: create({
-      brandTitle: "Mesosphere UI Kit",
-      brandUrl: "#",
-    }),
-  }
-});
-
-function loadStories() {
-  req.keys().forEach(filename => req(filename));
-}
-
-configure(loadStories, module);
