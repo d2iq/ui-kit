e843f04bedeed64d5b57b9192698f359799449e3
nataliepina <npina@d2iq.com>
2021-11-22 15:51:39 -0600

    feat: improve grid features and alignment options
    BREAKING CHANGE
    Before this fix columnCount was required, new behavior includes defaults for when it is not set to become a fluid grid that sets the amount of rows and columns based on available space.
    Before you could not center grid items, now the optional centerItems prop can be used.
    Other features include a data-cy prop, and className for further customizations.
    
    Closes D2IQ-81475

--- packages/styleUtils/layout/gridList/components/GridList.tsx
+++ packages/styleUtils/layout/gridList/components/GridList.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { css } from "@emotion/css";
+import { css, cx } from "@emotion/css";
 import { gridColumnTemplate } from "../style";
 import { SpaceSize } from "../../../../shared/styles/styleUtils/modifiers/modifierUtils";
 import {
@@ -8,46 +8,62 @@ import {
 } from "../../../../shared/styles/styleUtils";
 import { BreakpointConfig } from "../../../../shared/styles/breakpoints";
 
-interface GridListProps {
+export interface GridListProps {
   /**
-   * How many columns the grid should be. Can be set for all viewport sizes, or configured to have different values at different viewport width breakpoints
+   * Sets the amount of grid columns explicitly. Can be set for all viewport sizes, or configured to have different values at different viewport width breakpoints. If not set, automatic responsive grid styling will be in effect.
    */
-  columnCount: BreakpointConfig<number>;
+  columnCount?: BreakpointConfig<number>;
   /**
-   * The size of the space between each list item. Can be set for all viewport sizes, or configured to have different values at different viewport width breakpoints
+   * The size of the space (gap) between each list item. Can be set for all viewport sizes, or configured to have different values at different viewport width breakpoints. The default size is set to medium ("m" = 16px);
    */
   gutterSize?: SpaceSize;
   /**
-   * Which HTML list tag to render the component with
+   * Which HTML list tag to render the component with.
    */
-  tag: "ul" | "ol";
+  tag?: "ul" | "ol";
   children:
     | Array<React.ReactElement<HTMLLIElement>>
     | React.ReactElement<HTMLLIElement>;
+  /**
+   * Additional styles to extend the component.
+   */
+
+  className?: string;
+
+  /**
+   * Human-readable selector used for writing tests.
+   */
+  ["data-cy"]?: string;
+  /**
+   * Centers grid children by implementing `place-items: center;` Optimal for grids that could have a state with only 1 grid child.
+   */
+  centerItems?: boolean;
 }
 
-const GridList = (props: GridListProps) => {
-  const { children, columnCount, gutterSize, tag } = props;
+const GridList = ({
+  children,
+  columnCount,
+  gutterSize = "m",
+  tag = "ul",
+  centerItems = false,
+  className,
+  "data-cy": dataCy = "gridList"
+}: GridListProps) => {
   const GridListEl = tag;
 
+  const gridStyles = css`
+    display: grid;
+    ${getResponsiveSpacingStyle("grid-gap", gutterSize)};
+    ${gridColumnTemplate(columnCount)};
+    ${listReset};
+    place-items: ${centerItems ? "center" : "normal"};
+  `;
+
   return (
-    <GridListEl
-      className={css`
-        display: grid;
-        ${getResponsiveSpacingStyle("grid-gap", gutterSize)};
-        ${gridColumnTemplate(columnCount)};
-        ${listReset};
-      `}
-      data-cy="gridList"
-    >
+    <GridListEl className={cx(gridStyles, className)} data-cy={dataCy}>
       {children}
     </GridListEl>
   );
-};
-
-GridList.defaultProps = {
-  gutterSize: "m",
-  tag: "ul"
 };
 
 export default GridList;
