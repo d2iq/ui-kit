067218f7d2ca9c442161c92f842693e7ea586971
nataliepina <npina@d2iq.com>
2022-02-07 10:20:27 -0600

    docs: typography documentation

--- .storybook/docs/index.ts
+++ .storybook/docs/index.ts
@@ -1,4 +1,3 @@
 export { WelcomeCards } from "./components/WelcomeCards";
 export { WelcomeFooter } from "./components/WelcomeFooter";
 export { WelcomeBanner } from "./components/WelcomeBanner";
-export { ColorsTable } from "./components/Colors";
