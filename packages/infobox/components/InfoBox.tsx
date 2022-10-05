705b2f189da2039a8d602250845b7c10d3fee595
JohnRamberger <jramberger3@gatech.edu>
2022-10-05 14:54:12 -0400

    refactor: infobox component

--- packages/infobox/components/InfoBox.tsx
+++ packages/infobox/components/InfoBox.tsx
@@ -45,77 +45,73 @@ export interface InfoBoxProps {
   secondaryAction?: React.ReactElement<HTMLElement>;
 }
 
-const InfoBox = React.memo(
-  ({
-    appearance = "default",
-    message,
-    primaryAction,
-    secondaryAction,
-    onDismiss,
-    className
-  }: InfoBoxProps) => {
-    const hasActions = primaryAction || secondaryAction;
-    const isUrgentMessage = appearance === "danger" || appearance === "warning";
-    const dataCy = [
-      "infoBox",
-      ...(appearance && appearance !== "default"
-        ? [`infoBox.${appearance}`]
-        : [])
-    ].join(" ");
+const InfoBox = ({
+  appearance = "default",
+  message,
+  primaryAction,
+  secondaryAction,
+  onDismiss,
+  className
+}: InfoBoxProps) => {
+  const hasActions = primaryAction || secondaryAction;
+  const isUrgentMessage = appearance === "danger" || appearance === "warning";
+  const dataCy = [
+    "infoBox",
+    ...(appearance && appearance !== "default" ? [`infoBox.${appearance}`] : [])
+  ].join(" ");
 
-    const handleDismiss = (e): void => {
-      if (!onDismiss) {
-        return;
-      }
-      onDismiss(e);
-    };
+  const handleDismiss = (e): void => {
+    if (!onDismiss) {
+      return;
+    }
+    onDismiss(e);
+  };
 
-    return (
-      <div
-        className={cx(
-          infoBox(appearance),
-          padding("all"),
-          flush("bottom"),
-          textSize("s"),
-          textWeight("medium"),
-          className
-        )}
-        role={isUrgentMessage ? "alert" : "log"}
-        aria-live={isUrgentMessage ? "assertive" : "polite"}
-        data-cy={dataCy}
-      >
-        {hasActions ? (
-          <div className={infoBoxContentWrap}>
-            <div className={padding("bottom")}>{message}</div>
-            <div
-              className={cx(infoBoxActions, padding("bottom"))}
-              data-cy="infoBox-actions"
-            >
-              {primaryAction && (
-                <span className={cx(primaryActionStyle, padding("left"))}>
-                  {primaryAction}
-                </span>
-              )}
-
-              {secondaryAction && (
-                <span className={padding("left")}>{secondaryAction}</span>
-              )}
-            </div>
-          </div>
-        ) : (
+  return (
+    <div
+      className={cx(
+        infoBox(appearance),
+        padding("all"),
+        flush("bottom"),
+        textSize("s"),
+        textWeight("medium"),
+        className
+      )}
+      role={isUrgentMessage ? "alert" : "log"}
+      aria-live={isUrgentMessage ? "assertive" : "polite"}
+      data-cy={dataCy}
+    >
+      {hasActions ? (
+        <div className={infoBoxContentWrap}>
           <div className={padding("bottom")}>{message}</div>
-        )}
+          <div
+            className={cx(infoBoxActions, padding("bottom"))}
+            data-cy="infoBox-actions"
+          >
+            {primaryAction && (
+              <span className={cx(primaryActionStyle, padding("left"))}>
+                {primaryAction}
+              </span>
+            )}
 
-        {onDismiss && (
-          <Clickable action={handleDismiss} tabIndex={0}>
-            <span className={dismissBtn}>
-              <Icon shape={SystemIcons.Close} size="xs" color={greyDark} />
-            </span>
-          </Clickable>
-        )}
-      </div>
-    );
-  }
-);
+            {secondaryAction && (
+              <span className={padding("left")}>{secondaryAction}</span>
+            )}
+          </div>
+        </div>
+      ) : (
+        <div className={padding("bottom")}>{message}</div>
+      )}
 
-export default InfoBox;
+      {onDismiss && (
+        <Clickable action={handleDismiss} tabIndex={0}>
+          <span className={dismissBtn}>
+            <Icon shape={SystemIcons.Close} size="xs" color={greyDark} />
+          </span>
+        </Clickable>
+      )}
+    </div>
+  );
+};
+
+export default React.memo(InfoBox);
