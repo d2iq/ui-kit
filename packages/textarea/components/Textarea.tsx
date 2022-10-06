b20fc5a205228e4bef1fbc5392ebdea87c024c3e
Leonardo Maier <leonarmaier@gmail.com>
2022-10-06 15:07:10 -0300

    refactor: turn Textarea into a functional component (#795)

--- packages/textarea/components/Textarea.tsx
+++ packages/textarea/components/Textarea.tsx
@@ -19,15 +19,15 @@ export interface TextareaProps extends React.HTMLProps<HTMLTextAreaElement> {
   /**
    * Sets the current appearance of the component. This defaults to InputAppearance.Standard, but supports `InputAppearance.Error` & `InputAppearance.Success` appearances as well.
    */
-  appearance: InputAppearance;
+  appearance?: InputAppearance;
   /**
    * Sets the contents of the label. This can be a `string` or any `ReactNode`.
    */
-  inputLabel: React.ReactNode;
+  inputLabel?: React.ReactNode;
   /**
    * Defaults to `true`, but can be set to `false` to visibly hide the `Textarea`'s label. The `inputLabel` should still be set even when hidden for accessibility support.
    */
-  showInputLabel: boolean;
+  showInputLabel?: boolean;
   /**
    * Text or a ReactNode that is displayed directly under the textarea with additional information about the expected input.
    */
@@ -42,79 +42,74 @@ export interface TextareaProps extends React.HTMLProps<HTMLTextAreaElement> {
   tooltipContent?: React.ReactNode;
 }
 
-class Textarea extends React.PureComponent<TextareaProps, {}> {
-  public static defaultProps: Partial<TextareaProps> = {
-    appearance: InputAppearance.Standard,
-    showInputLabel: true,
-    rows: 3
-  };
+const Textarea = ({
+  id = nextId("textarea-"),
+  appearance = InputAppearance.Standard,
+  inputLabel,
+  showInputLabel = true,
+  errors,
+  hintContent,
+  tooltipContent,
+  value,
+  rows = 3,
+  required,
+  disabled,
+  ...other
+}: TextareaProps) => {
+  const hasError = appearance === InputAppearance.Error;
 
-  id = this.props.id ?? nextId("textarea-");
+  const getInputAppearance = () => (disabled ? "disabled" : appearance);
 
-  public render() {
-    const {
-      appearance,
-      errors,
-      hintContent,
-      id,
-      inputLabel,
-      showInputLabel,
-      tooltipContent,
-      value,
-      ...other
-    } = this.props;
-    const hasError = appearance === InputAppearance.Error;
-    let { onChange } = other;
-    const inputAppearance = this.getInputAppearance();
-    if (onChange == null && value != null) {
-      onChange = Function.prototype as (
-        event: React.FormEvent<HTMLTextAreaElement>
-      ) => void;
-    }
-    const parentDataCy = `textarea textarea.${inputAppearance}`;
-    const textareaDataCy = `textarea-textarea textarea-textarea.${inputAppearance}`;
+  let { onChange } = other;
 
-    return (
-      <FormFieldWrapper id={id} errors={errors} hintContent={hintContent}>
-        {({ getValidationErrors, getHintContent, isValid, describedByIds }) => (
-          <div data-cy={parentDataCy}>
-            {renderLabel({
-              appearance,
-              hidden: !showInputLabel,
-              id: this.id,
-              label: inputLabel,
-              required: this.props.required,
-              tooltipContent
-            })}
+  const inputAppearance = getInputAppearance();
 
-            <textarea
-              aria-invalid={!isValid}
-              aria-describedby={describedByIds}
-              value={value}
-              id={id}
-              className={cx(
-                inputReset,
-                inputContainer,
-                getInputAppearanceStyle(this.getInputAppearance()),
-                textarea
-              )}
-              required={this.props.required}
-              data-cy="textarea-textarea"
-              {...{ ...other, onChange }}
-            />
-            <div data-cy={textareaDataCy}>
-              {getHintContent}
-              {hasError ? getValidationErrors : null}
-            </div>
+  if (onChange == null && value != null) {
+    onChange = Function.prototype as (
+      event: React.FormEvent<HTMLTextAreaElement>
+    ) => void;
+  }
+
+  const parentDataCy = `textarea textarea.${inputAppearance}`;
+
+  const textareaDataCy = `textarea-textarea textarea-textarea.${inputAppearance}`;
+
+  return (
+    <FormFieldWrapper id={id} errors={errors} hintContent={hintContent}>
+      {({ getValidationErrors, getHintContent, isValid, describedByIds }) => (
+        <div data-cy={parentDataCy}>
+          {renderLabel({
+            appearance,
+            hidden: !showInputLabel,
+            id,
+            label: inputLabel,
+            required,
+            tooltipContent
+          })}
+
+          <textarea
+            aria-invalid={!isValid}
+            aria-describedby={describedByIds}
+            value={value}
+            id={id}
+            className={cx(
+              inputReset,
+              inputContainer,
+              getInputAppearanceStyle(getInputAppearance()),
+              textarea
+            )}
+            required={required}
+            data-cy="textarea-textarea"
+            {...{ ...other, rows, onChange }}
+          />
+          <div data-cy={textareaDataCy}>
+            {getHintContent}
+            {hasError ? getValidationErrors : null}
           </div>
-        )}
-      </FormFieldWrapper>
-    );
-  }
+        </div>
+      )}
+    </FormFieldWrapper>
+  );
+};
 
-  private getInputAppearance(): string {
-    return this.props.disabled ? "disabled" : this.props.appearance;
-  }
-}
-
-export default Textarea;
+export default React.memo(Textarea);
