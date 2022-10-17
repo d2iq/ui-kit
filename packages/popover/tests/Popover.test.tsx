5d9b113e38b8ede00bb3be9104b8fd85ff15329e
Leonardo Maier <leonarmaier@gmail.com>
2022-10-17 06:18:10 -0300

    refactor: update Popover component tests from enzyme to react-testing-library (#821)

--- packages/popover/tests/Popover.test.tsx
+++ packages/popover/tests/Popover.test.tsx
@@ -1,9 +1,8 @@
 import React from "react";
-import { mount } from "enzyme";
 import { createSerializer } from "@emotion/jest";
-import toJson from "enzyme-to-json";
 
 import Popover from "../components/Popover";
+import { render, fireEvent } from "@testing-library/react";
 
 expect.addSnapshotSerializer(createSerializer());
 
@@ -11,116 +10,122 @@ const triggerId = "trigger";
 
 describe("Popover", () => {
   it("renders a closed dropdown", () => {
-    const component = mount(
+    const { asFragment } = render(
       <Popover trigger="Dropdown trigger">
         <div>dropdown content</div>
       </Popover>
     );
 
-    expect(toJson(component)).toMatchSnapshot();
+    expect(asFragment()).toMatchSnapshot();
   });
   it("renders an open dropdown", () => {
-    const component = mount(
+    const { asFragment } = render(
       <Popover initialIsOpen={true} trigger="Dropdown trigger">
         <div>dropdown content</div>
       </Popover>
     );
 
-    expect(toJson(component)).toMatchSnapshot();
+    expect(asFragment()).toMatchSnapshot();
   });
   it("renders a dropdown with a max width and max height", () => {
-    const component = mount(
+    const { asFragment } = render(
       <Popover trigger="Dropdown trigger" maxHeight={50} maxWidth={50}>
         <div>dropdown content</div>
       </Popover>
     );
 
-    expect(toJson(component)).toMatchSnapshot();
+    expect(asFragment()).toMatchSnapshot();
   });
 
   it("renders an element passed as to the trigger prop", () => {
-    const component = mount(
-      <Popover trigger={<div id={triggerId}>Dropdown trigger</div>}>
+    const { getByTestId } = render(
+      <Popover trigger={<div data-cy={triggerId}>Dropdown trigger</div>}>
         <div>dropdown content</div>
       </Popover>
     );
 
-    expect(component.find(`#${triggerId}`).exists()).toBe(true);
+    expect(getByTestId(triggerId)).toBeTruthy();
   });
   it("toggles the dropdown on click", () => {
-    const component = mount(
-      <Popover trigger={<div id={triggerId}>Dropdown trigger</div>}>
+    const { getByTestId } = render(
+      <Popover trigger={<div data-cy={triggerId}>Dropdown trigger</div>}>
         <div>dropdown content</div>
       </Popover>
     );
-    const trigger = component.find(`#${triggerId}`);
 
-    expect(component.find(`#${triggerId}`).prop("aria-expanded")).toBe(false);
-    trigger.simulate("click");
-    expect(component.find(`#${triggerId}`).prop("aria-expanded")).toBe(true);
-    trigger.simulate("click");
-    expect(component.find(`#${triggerId}`).prop("aria-expanded")).toBe(false);
+    const triggerElement = getByTestId(triggerId);
+
+    expect(triggerElement.getAttribute("aria-expanded")).toBe("false");
+
+    triggerElement.click();
+
+    expect(triggerElement.getAttribute("aria-expanded")).toBe("true");
+
+    triggerElement.click();
+
+    expect(triggerElement.getAttribute("aria-expanded")).toBe("false");
   });
   it("opens the dropdown menu when focusing and pressing the spacebar, and closes it on blur", () => {
-    const component = mount(
-      <Popover trigger={<div id={triggerId}>Dropdown trigger</div>}>
+    const { getByTestId } = render(
+      <Popover trigger={<div data-cy={triggerId}>Dropdown trigger</div>}>
         <div>dropdown content</div>
       </Popover>
     );
-    const trigger = component.find(`#${triggerId}`);
+    const triggerElement = getByTestId(triggerId);
 
-    expect(component.find(`#${triggerId}`).prop("aria-expanded")).toBe(false);
+    expect(triggerElement.getAttribute("aria-expanded")).toBe("false");
 
-    trigger.simulate("focus");
-    trigger.simulate("keyDown", {
-      key: " " // space bar
+    triggerElement.focus();
+
+    fireEvent.keyDown(triggerElement, {
+      key: " "
     });
 
-    expect(component.find(`#${triggerId}`).prop("aria-expanded")).toBe(true);
+    expect(triggerElement.getAttribute("aria-expanded")).toBe("true");
 
-    trigger.simulate("blur");
+    triggerElement.blur();
 
     // setTimeout needed because there is a setTimeout in the onBlur handler
     setTimeout(() => {
-      expect(component.find(`#${triggerId}`).prop("aria-expanded")).toBe(false);
+      expect(triggerElement.getAttribute("aria-expanded")).toBe("false");
     }, 0);
   });
   it("opens the dropdown menu when focusing and pressing the down arrow", () => {
-    const component = mount(
-      <Popover trigger={<div id={triggerId}>Dropdown trigger</div>}>
+    const { getByTestId } = render(
+      <Popover trigger={<div data-cy={triggerId}>Dropdown trigger</div>}>
         <div>dropdown content</div>
       </Popover>
     );
-    const trigger = component.find(`#${triggerId}`);
+    const triggerElement = getByTestId(triggerId);
 
-    expect(component.find(`#${triggerId}`).prop("aria-expanded")).toBe(false);
+    expect(triggerElement.getAttribute("aria-expanded")).toBe("false");
 
-    trigger.simulate("focus");
-    trigger.simulate("keyDown", {
+    triggerElement.focus();
+    fireEvent.keyDown(triggerElement, {
       key: "ArrowDown"
     });
 
-    expect(component.find(`#${triggerId}`).prop("aria-expanded")).toBe(true);
+    expect(triggerElement.getAttribute("aria-expanded")).toBe("true");
   });
 
   it("closes the dropdown when keying Escape", () => {
-    const component = mount(
-      <Popover trigger={<div id={triggerId}>Dropdown trigger</div>}>
+    const { getByTestId } = render(
+      <Popover trigger={<div data-cy={triggerId}>Dropdown trigger</div>}>
         <div>dropdown content</div>
       </Popover>
     );
-    const trigger = component.find(`#${triggerId}`);
+    const triggerElement = getByTestId(triggerId);
 
-    expect(component.find(`#${triggerId}`).prop("aria-expanded")).toBe(false);
-    trigger.simulate("click");
-    expect(component.find(`#${triggerId}`).prop("aria-expanded")).toBe(true);
-    trigger.simulate("keyDown", {
+    expect(triggerElement.getAttribute("aria-expanded")).toBe("false");
+    triggerElement.click();
+    expect(triggerElement.getAttribute("aria-expanded")).toBe("true");
+    fireEvent.keyDown(triggerElement, {
       key: "Escape"
     });
 
     // setTimeout needed because there is a setTimeout in the onBlur handler
     setTimeout(() => {
-      expect(component.find(`#${triggerId}`).prop("aria-expanded")).toBe(false);
+      expect(triggerElement.getAttribute("aria-expanded")).toBe("false");
     }, 0);
   });
 });
