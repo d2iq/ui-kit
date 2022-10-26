7434c1143f4e427f52fb736d06cf3fc5f3fbbdd0
Russell Anderson <randerson@d2iq.com>
2022-10-26 16:16:09 -0500

    test: clean up toast test suite (#866)
    * test: clean up toast test suite
    
    The toast test suite was tightly coupled with Toaster. This may have
    been a copy paste issue or something, but the snapshots didn't even
    reflect the markup of the Toast component. I have removed the wrapping
    of each of these in Toaster, and a couple of tests that were targeting
    behavior of Toaster rather than Toast. I have refactored to use RTL
    instead of enzyme in other cases.
    
    * refactor: toast to function

--- packages/toaster/components/Toast.tsx
+++ packages/toaster/components/Toast.tsx
@@ -26,7 +26,7 @@ export interface ToastProps {
   title: React.ReactElement<HTMLElement> | string;
   description?: React.ReactElement<HTMLElement> | string;
   id: ToastId;
-  appearance: "default" | "success" | "warning" | "danger";
+  appearance?: "default" | "success" | "warning" | "danger";
   autodismiss?: boolean;
   onDismiss?: (event?: React.SyntheticEvent<HTMLElement>) => void;
   dismissToast?: (dismissedToastId: ToastId) => void;
@@ -54,100 +54,93 @@ const getIconForAppearance = (appearance: ToastProps["appearance"]) => {
   }
 };
 
-class Toast extends React.PureComponent<ToastProps, {}> {
-  public static defaultProps: Partial<ToastProps> = {
-    appearance: "default",
-    autodismiss: false
-  };
+const Toast = ({
+  title,
+  description,
+  appearance = "default",
+  primaryAction,
+  secondaryAction,
+  dismissToast,
+  onDismiss,
+  id
+}: ToastProps) => {
+  const isUrgentMessage = appearance === "danger" || appearance === "warning";
+  const dataCy = `toast toast.${appearance}`;
 
-  constructor(props) {
-    super(props);
-
-    this.handleDismiss = this.handleDismiss.bind(this);
-  }
-
-  public render() {
-    const { title, description, appearance, primaryAction, secondaryAction } =
-      this.props;
-    const isUrgentMessage = appearance === "danger" || appearance === "warning";
-    const dataCy = `toast toast.${appearance}`;
-
-    return (
-      <div
-        className={cx(
-          toast,
-          inverseColorMode,
-          margin("bottom", "xxs"),
-          padding("top", "xs"),
-          padding("right", "xs"),
-          padding("left", "xs"),
-          display("grid")
-        )}
-        role={isUrgentMessage ? "alert" : "log"}
-        aria-relevant="all"
-        aria-atomic="true"
-        data-cy={dataCy}
-      >
-        <div
-          className={cx(
-            toastTitle,
-            flex({ align: "center" }),
-            padding("bottom", "xs")
-          )}
-        >
-          {appearance !== "default" && (
-            <div
-              className={cx(
-                display("inherit"),
-                flexItem("shrink"),
-                padding("right", "s")
-              )}
-            >
-              {getIconForAppearance(appearance)}
-            </div>
-          )}
-          <div className={flexItem("grow")}>{title}</div>
-        </div>
-        {description && (
-          <div
-            className={cx(toastDesc, padding("bottom", "xs"))}
-            data-cy="toast-description"
-          >
-            {description}
-          </div>
-        )}
-        {(primaryAction || secondaryAction) && (
-          <div
-            className={cx(
-              toastActions,
-              flex({ direction: "row-reverse" }),
-              padding("bottom", "xs")
-            )}
-            data-cy="toast-actions"
-          >
-            {primaryAction && (
-              <span className={padding("left")}>{primaryAction}</span>
-            )}
-            {secondaryAction && <span>{secondaryAction}</span>}
-          </div>
-        )}
-        <Clickable tabIndex={0} action={this.handleDismiss}>
-          <span className={toastDismiss}>
-            <Icon shape={SystemIcons.Close} color="inherit" size="xs" />
-          </span>
-        </Clickable>
-      </div>
-    );
-  }
-
-  private handleDismiss(e) {
-    const { dismissToast, onDismiss, id } = this.props;
+  const handleDismiss = e => {
     if (dismissToast) {
       dismissToast(id);
     } else if (onDismiss) {
       onDismiss(e);
     }
-  }
-}
+  };
 
-export default Toast;
+  return (
+    <div
+      className={cx(
+        toast,
+        inverseColorMode,
+        margin("bottom", "xxs"),
+        padding("top", "xs"),
+        padding("right", "xs"),
+        padding("left", "xs"),
+        display("grid")
+      )}
+      role={isUrgentMessage ? "alert" : "log"}
+      aria-relevant="all"
+      aria-atomic="true"
+      data-cy={dataCy}
+    >
+      <div
+        className={cx(
+          toastTitle,
+          flex({ align: "center" }),
+          padding("bottom", "xs")
+        )}
+      >
+        {appearance !== "default" && (
+          <div
+            className={cx(
+              display("inherit"),
+              flexItem("shrink"),
+              padding("right", "s")
+            )}
+          >
+            {getIconForAppearance(appearance)}
+          </div>
+        )}
+        <div className={flexItem("grow")}>{title}</div>
+      </div>
+      {description && (
+        <div
+          className={cx(toastDesc, padding("bottom", "xs"))}
+          data-cy="toast-description"
+        >
+          {description}
+        </div>
+      )}
+      {(primaryAction || secondaryAction) && (
+        <div
+          className={cx(
+            toastActions,
+            flex({ direction: "row-reverse" }),
+            padding("bottom", "xs")
+          )}
+          data-cy="toast-actions"
+        >
+          {primaryAction && (
+            <span className={padding("left")}>{primaryAction}</span>
+          )}
+          {secondaryAction && <span>{secondaryAction}</span>}
+        </div>
+      )}
+      <Clickable tabIndex={0} action={handleDismiss}>
+        <span className={toastDismiss}>
+          <Icon shape={SystemIcons.Close} color="inherit" size="xs" />
+        </span>
+      </Clickable>
+    </div>
+  );
+};
+
+export default React.memo(Toast);
