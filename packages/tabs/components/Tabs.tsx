6bb5ebec3e51782629aba79049efdaeed41eb2e9
Max Strübing <mxstrbng@gmail.com>
2022-11-25 13:31:33 +0100

    Fix Tab Component again (#886)
    * Revert "fix: use react-id-generator (#885)"
    
    This reverts commit a2622c67eba89d3ecf18645dbf7751d4d3317102.
    
    * Revert "fix: duplicate key in tabs component (#882)"
    
    This reverts commit 6d0d54d313b09ea1e5cf1a83e80df4b98f886845.
    
    * fix: use index as key to be not newly generated all the time

--- packages/tabs/components/Tabs.tsx
+++ packages/tabs/components/Tabs.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { Tabs as ReactTabs, TabList, TabPanel } from "react-tabs";
 import { injectGlobal, cx } from "@emotion/css";
-import nextId from "react-id-generator";
+
 import { TabItemProps } from "./TabItem";
 import { TabTitle } from "..";
 import {
@@ -13,7 +13,9 @@ import {
 import { listReset } from "../../shared/styles/styleUtils";
 import { BreakpointConfig } from "../../shared/styles/breakpoints";
 import { fullHeightTabs, getTabLayout } from "../style";
+
 export const defaultTabDirection = "horiz";
+
 // Copy & paste from node_modules/react-tabs/style/react-tabs.css
 // Also changed to better fit the ui kit styles.
 // This is needed to give the tabs a style
@@ -22,14 +24,17 @@ injectGlobal`
 .react-tabs {
   -webkit-tap-highlight-color: transparent;
 }
+
 .react-tabs__tab-list {
   ${listReset};
 }
+
 .react-tabs__tab {
   position: relative;
   cursor: pointer;
   font-weight: ${fontWeightMedium};
   color: ${themeTextColorPrimary};
+
   &:after{
     content: "";
     position: absolute;
@@ -37,29 +42,35 @@ injectGlobal`
     display: none;
   }
 }
+
 .react-tabs__tab:focus {
   outline: none;
   background: ${themeBgHover}
 }
+
 .react-tabs__tab--selected {
   &:after{
     display: block;
   }
   color: ${themeBrandPrimary};
 }
+
 .react-tabs__tab--disabled {
   color: GrayText;
   cursor: default;
 }
+
 .react-tabs__tab-panel {
   display: none;
   flex-grow: 1;
 }
+
 .react-tabs__tab-panel--selected {
   display: block;
 }
 `;
 /* eslint-enable */
+
 export type TabDirections = "horiz" | "vert";
 export type TabDirection = BreakpointConfig<TabDirections>;
 export type TabSelected = string;
@@ -71,55 +82,53 @@ export interface TabsProps {
   onSelect?: (tabIndex: number) => void;
   direction?: TabDirection;
 }
+
 const Tabs = ({
   children,
   selectedIndex,
   onSelect,
   direction = defaultTabDirection
 }: TabsProps) => {
-  const { tabs, tabsContent } = React.useMemo(() => {
-    return (
-      React.Children.toArray(children) as Array<
-        React.ReactElement<TabItemProps>
-      >
-    )
-      .filter(item => React.isValidElement<TabItemProps>(item))
-      .reduce<{
-        tabs: React.ReactNode[];
-        tabsContent: React.ReactNode[];
-      }>(
-        (acc, item) => {
-          const { tabs = [], tabsContent = [] } = acc;
-          const { children } = item.props;
-          const key = item.key ? item.key : undefined;
-          const childrenWithKeys = React.Children.toArray(children).map(
-            child => {
-              return React.isValidElement<typeof TabTitle>(child)
-                ? React.cloneElement(child, { key: nextId("tab") })
-                : child;
-            }
-          );
-          const title = childrenWithKeys.find(
-            child =>
-              React.isValidElement<typeof TabTitle>(child) &&
-              child.type === TabTitle
-          );
-          const tabChildren = childrenWithKeys.filter(
-            child => !(React.isValidElement(child) && child.type === TabTitle)
-          );
-          return {
-            tabs: [...tabs, title],
-            tabsContent: [
-              ...tabsContent,
-              ...(tabChildren.length
-                ? [<TabPanel key={key}>{tabChildren}</TabPanel>]
-                : [])
-            ]
-          };
-        },
-        { tabs: [], tabsContent: [] }
-      );
-  }, [children]);
+  const { tabs, tabsContent } = (
+    React.Children.toArray(children) as Array<React.ReactElement<TabItemProps>>
+  )
+    .filter(item => React.isValidElement<TabItemProps>(item))
+    .reduce<{
+      tabs: React.ReactNode[];
+      tabsContent: React.ReactNode[];
+    }>(
+      (acc, item) => {
+        const { tabs = [], tabsContent = [] } = acc;
+        const { children } = item.props;
+        const key = item.key ? item.key : undefined;
+        const childrenWithKeys = React.Children.toArray(children).map(
+          (child, index) =>
+            React.isValidElement<typeof TabTitle>(child)
+              ? React.cloneElement(child, { key: `${key}-${index}` })
+              : child
+        );
+
+        const title = childrenWithKeys.find(
+          child =>
+            React.isValidElement<typeof TabTitle>(child) &&
+            child.type === TabTitle
+        );
+        const tabChildren = childrenWithKeys.filter(
+          child => !(React.isValidElement(child) && child.type === TabTitle)
+        );
+        return {
+          tabs: [...tabs, title],
+          tabsContent: [
+            ...tabsContent,
+            ...(tabChildren.length
+              ? [<TabPanel key={key}>{tabChildren}</TabPanel>]
+              : [])
+          ]
+        };
+      },
+      { tabs: [], tabsContent: [] }
+    );
+
   return (
     <ReactTabs
       className={cx("react-tabs", {
@@ -138,4 +147,5 @@ const Tabs = ({
     </ReactTabs>
   );
 };
+
 export default React.memo(Tabs);
