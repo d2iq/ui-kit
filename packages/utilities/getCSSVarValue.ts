74d355c4f107c8717e31fb09e2b1a99620ab0ac6
Russell Anderson <randerson@d2iq.com>
2022-12-13 09:15:52 -0600

    fix: make var check ssr friendly (#908)
    document and window are not defined in node environments, so trying to
    use ui-kit with an isomorphic framework like next will fail saying
    document/window is not defined. Adding a check that window is not
    undefined before trying to operate on document seems to fix the issue.
    There may be more places like this in ui-kit but this is all I've run up
    against so far.

--- packages/utilities/getCSSVarValue.ts
+++ packages/utilities/getCSSVarValue.ts
@@ -1,10 +1,14 @@
 const getCSSVarValue = cssVar => {
-  if (document.documentElement !== null) {
-    // Get the content between the parentheses in "var()"
-    // and split the args from a string to an array
-    const cssVarArgsString = cssVar.match(/\(([^)]+)\)/)[1];
-    const cssVarArgs = cssVarArgsString.replace(/\s/g, "").split(",");
+  // Get the content between the parentheses in "var()"
+  // and split the args from a string to an array
+  const cssVarArgsString = cssVar.match(/\(([^)]+)\)/)[1];
+  const cssVarArgs = cssVarArgsString.replace(/\s/g, "").split(",");
 
+  if (typeof window === "undefined") {
+    return cssVarArgs[1];
+  }
+
+  if (document?.documentElement !== null) {
     // If there's a custom property defined on :root (<html>), return that.
     // Otherwise, return the fallback value.
     return (
