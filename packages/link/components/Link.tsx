37a416af7a84b8464167d31cd31f7d22585b776a
davidtaing <adavidtaing@gmail.com>
2022-12-15 19:56:36 +1100

    feat: add className prop to Link
    to allow custom style overrides

--- packages/link/components/Link.tsx
+++ packages/link/components/Link.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { cx } from "@emotion/css";
 import ResetLink from "./ResetLink";
 import { ExpandedLinkProps } from "../types";
 import { defaultLink } from "../style";
 
 const Link = ({ children, className, ...other }: ExpandedLinkProps) => (
-  <ResetLink className={defaultLink} {...other}>
+  <ResetLink className={cx(defaultLink, className)} {...other}>
     {children}
   </ResetLink>
 );
