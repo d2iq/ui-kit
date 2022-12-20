405aacf2bc6a35f906965b740d7ab2f0bb265cba
Natalie Pina <34781875+nataliepina@users.noreply.github.com>
2022-12-20 13:40:46 -0600

    feat: inlineloadingindicator custom props (#911)
    Introduces custom color, size, and data-cy props.

--- packages/loadingIndicator/components/InlineLoadingIndicator.tsx
+++ packages/loadingIndicator/components/InlineLoadingIndicator.tsx
@@ -1,12 +1,31 @@
 import * as React from "react";
 import { Icon } from "../../icon";
 import { SystemIcons } from "../../icons/dist/system-icons-enum";
+import { IconSize } from "../../shared/types/iconSize";
 
-const InlineLoadingIndicator = () => (
+const DEFAULT_ICON_SIZE: IconSize = "xs";
+
+export interface InlineLoadingIndicatorProps {
+  /**
+   * The fill color of the spinner.
+   */
+  color?: string;
+  /** Which icon size to use for the width and height of the icon. */
+  size?: IconSize;
+  /** Human-readable selector used for writing tests. */
+  "data-cy"?: string;
+}
+
+const InlineLoadingIndicator = ({
+  size = DEFAULT_ICON_SIZE,
+  "data-cy": dataCy = "inlineLoadingIndicator",
+  color = "inherit"
+}) => (
   <Icon
     shape={SystemIcons.Spinner}
-    size="xs"
-    data-cy="inlineLoadingIndicator"
+    size={size}
+    data-cy={dataCy}
+    color={color}
   />
 );
 
