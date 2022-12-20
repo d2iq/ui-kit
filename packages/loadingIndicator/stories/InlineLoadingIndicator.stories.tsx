b322b6bf0e7714ed8d7dcf4ca051fe94c52397ba
nataliepina <npina@d2iq.com>
2022-12-20 12:32:13 -0600

    feat: inlineloadingindicator custom props
    Introduces custom color, size, and data-cy props.

--- packages/loadingIndicator/stories/InlineLoadingIndicator.stories.tsx
+++ packages/loadingIndicator/stories/InlineLoadingIndicator.stories.tsx
@@ -9,33 +9,48 @@ import {
 } from "../../configurationmap";
 
 import { InlineLoadingIndicator } from "..";
+import { spacingSizeValues } from "../../storybookHelpers/controlConstants";
 
 export default {
   title: "Feedback/Loading Indicators/Inline Loading Indicator",
-  component: InlineLoadingIndicator
+  component: InlineLoadingIndicator,
+  argTypes: {
+    color: {
+      control: { type: "color" }
+    },
+    size: {
+      options: spacingSizeValues,
+      control: {
+        type: "select"
+      }
+    },
+    "data-cy": {
+      control: { disable: true }
+    }
+  }
 } as Meta;
 
-const Template: Story = args => (
-  <>
-    <ConfigurationMap>
-      <ConfigurationMapSection>
-        <ConfigurationMapRow>
-          <ConfigurationMapLabel>Name</ConfigurationMapLabel>
-          <ConfigurationMapValue>
-            <InlineLoadingIndicator {...args} />
-          </ConfigurationMapValue>
-        </ConfigurationMapRow>
-        <ConfigurationMapRow>
-          <ConfigurationMapLabel>Role</ConfigurationMapLabel>
-          <ConfigurationMapValue>UX Designer</ConfigurationMapValue>
-        </ConfigurationMapRow>
-        <ConfigurationMapRow>
-          <ConfigurationMapLabel>City</ConfigurationMapLabel>
-          <ConfigurationMapValue>San Francisco</ConfigurationMapValue>
-        </ConfigurationMapRow>
-      </ConfigurationMapSection>
-    </ConfigurationMap>
-  </>
-);
+const Template: Story = args => <InlineLoadingIndicator {...args} />;
 
 export const Default = Template.bind({});
+
+export const InlineWithinComponent = args => (
+  <ConfigurationMap>
+    <ConfigurationMapSection>
+      <ConfigurationMapRow>
+        <ConfigurationMapLabel>Name</ConfigurationMapLabel>
+        <ConfigurationMapValue>
+          <InlineLoadingIndicator {...args} />
+        </ConfigurationMapValue>
+      </ConfigurationMapRow>
+      <ConfigurationMapRow>
+        <ConfigurationMapLabel>Role</ConfigurationMapLabel>
+        <ConfigurationMapValue>UX Designer</ConfigurationMapValue>
+      </ConfigurationMapRow>
+      <ConfigurationMapRow>
+        <ConfigurationMapLabel>City</ConfigurationMapLabel>
+        <ConfigurationMapValue>San Francisco</ConfigurationMapValue>
+      </ConfigurationMapRow>
+    </ConfigurationMapSection>
+  </ConfigurationMap>
+);
