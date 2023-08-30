3965228b4e9dff4560cd473534e35bc32d520cc4
Natalie Pina <34781875+nataliepina@users.noreply.github.com>
2023-08-30 08:45:14 -0500

    Storybook files to TS (#996)
    * chore:  update storybook files to typescript
    
    * chore(deps): updated versioning

---
+++ .storybook/main.ts
@@ -0,0 +1,36 @@
+import type { StorybookConfig } from "@storybook/react-webpack5";
+
+const customWebpack = require("./webpack.config");
+
+const config: StorybookConfig = {
+  // Required
+  framework: "@storybook/react-webpack5",
+  webpackFinal: config => {
+    return {
+      ...config,
+      ...customWebpack
+    };
+  },
+  stories: [
+    "./docs/stories/Welcome.stories.mdx",
+    "./docs/stories/Colors.stories.mdx",
+    "./docs/stories/Typography.stories.mdx",
+    "../packages/**/*.stories.@(tsx|mdx)"
+  ],
+  // Optional
+  addons: [
+    {
+      name: "@storybook/addon-essentials",
+      options: {
+        actions: false
+      }
+    },
+    "@storybook/addon-a11y"
+  ],
+  docs: {
+    autodocs: true
+  },
+  staticDirs: ["../public/assets/images"]
+};
+
+export default config;
