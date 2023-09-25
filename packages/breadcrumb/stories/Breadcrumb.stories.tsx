b65a73f3e01005e11b2fb09db0b7d87c3e699e49
nataliepina <npina@d2iq.com>
2023-09-25 10:54:19 -0500

    docs: access to all product icons for breadcrumb

--- packages/breadcrumb/stories/Breadcrumb.stories.tsx
+++ packages/breadcrumb/stories/Breadcrumb.stories.tsx
@@ -5,17 +5,25 @@ import { action } from "@storybook/addon-actions";
 import { ProductIcons } from "../../icons/dist/product-icons-enum";
 import { BreadcrumbProps } from "../components/Breadcrumb";
 
+// Filter out "Inverse" product icons for this story
+const filteredProductIcons = Object.keys(ProductIcons)
+  .filter(key => !key.includes("Inverse"))
+  .reduce((obj, key) => {
+    obj[key] = ProductIcons[key];
+    return obj;
+  }, {});
+
 export default {
   title: "Navigation/Breadcrumb",
   component: BreadcrumbItem,
   subcomponents: { Breadcrumb },
   argTypes: {
     icon: {
-      options: [ProductIcons.Gear, ProductIcons.Cluster, ProductIcons.Users],
-      type: "select"
+      options: Object.keys(filteredProductIcons),
+      mapping: filteredProductIcons
     }
   }
-};
+} as Meta;
 
 const Template: StoryFn<BreadcrumbProps> = args => (
   <Breadcrumb>
